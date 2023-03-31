c5b7892af4ee2040ef414cfb3add77f8d4b16761
hojae29 <gkqthd6386@naver.com>
2023-03-31 17:34:45 +0900

    Spring MVC (스프링 웹 MVC) 강의 11 - 공지사항 컨트롤러 추가하기

---
+++ src/main/java/com/newlecture/web/controller/notice/ListController.java
@@ -0,0 +1,18 @@
+package com.newlecture.web.controller.notice;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ListController implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        ModelAndView mv = new ModelAndView("notice/list");
+
+        return mv;
+    }
+}
