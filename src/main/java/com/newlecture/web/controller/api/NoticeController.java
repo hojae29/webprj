63aa98ebe4ac630688bafd0345fb75662e516f7e
hojae29 <gkqthd6386@naver.com>
2023-04-05 10:47:27 +0900

    Spring MVC (스프링 웹 MVC) 강의 33 - JSON 출력하기

--- src/main/java/com/newlecture/web/controller/api/NoticeController.java
+++ src/main/java/com/newlecture/web/controller/api/NoticeController.java
@@ -1,15 +1,27 @@
 package com.newlecture.web.controller.api;
 
+import com.newlecture.web.entity.Notice;
+import com.newlecture.web.service.NoticeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
+import java.util.List;
 
 // 프로젝트 내에 NoticeController가 2개 있기 때문에 이름 설정
 @RestController("apiNoticeController")
 @RequestMapping("/api/notice")
 public class NoticeController {
 
+    @Autowired
+    private NoticeService noticeService;
+
     @RequestMapping("/list")
-    public String list() {
-        return "공지사항 list";
+    public List<Notice> list() throws SQLException, ClassNotFoundException {
+
+        List<Notice> list = noticeService.getList(1, "title", "");
+
+        return list;
     }
 }
